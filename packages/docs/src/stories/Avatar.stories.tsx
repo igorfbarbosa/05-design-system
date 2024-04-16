9968730eeff9bba3ea4c3d444b536ff2e780c895
igorfbarbosa <igorfbarbosa@gmail.com>
2024-04-16 10:31:26 -0300

    Componentes do Design System - Componente: Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://pbs.twimg.com/profile_images/1531426288812146688/t8uRVjj4_400x400.jpg',
+    alt: 'Igor Barbosa',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
